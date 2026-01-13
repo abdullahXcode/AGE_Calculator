95f1660a2e41943f013257eb65dd9605d6c36395
Muhammad Abdullah <abdullah596239@gmail.com>
2026-01-14 00:40:53 +0500

    Change days_in_a_month from int to void return type

--- AGE_CALCULATOR.cpp
+++ AGE_CALCULATOR.cpp
@@ -35,7 +35,7 @@ bool is_leap_year(int year)
     }
 }
 
-int days_in_a_month(int month, int year)
+void days_in_a_month(int month, int year)
 {
     if(month == 1 or month == 3 or month == 5 or
        month == 7 or month == 8 or month == 10 or month == 12)
